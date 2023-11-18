63bf0f533f00b4718e7919b8f169ef648fa95668
walidwahbaa <walidwahba5000@gmail.com>
2023-11-18 11:03:05 +0300

    make the card collection view cell and then apply the protocols and delegete on the view controller to make the collection view

---
+++ MatchGame/CardCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CardCollectionViewCell.swift
+//  MatchGame
+//
+//  Created by walid wahba on 17.11.2023.
+//
+
+import UIKit
+
+class CardCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var frontImageView: UIImageView!
+    
+    
+    
+    @IBOutlet weak var backImageView: UIImageView!
+}
--- MatchGame/ViewController.swift
+++ MatchGame/ViewController.swift
@@ -7,7 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+//We want to set the view controller as the object that is gonna be suppling the data to the collecetion view & responsable for handling the user  interaction events from the collection view
+class ViewController: UIViewController , UICollectionViewDataSource,UICollectionViewDelegate {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     var model = CardModel()
     var cardsArray = [Card]()
     
@@ -15,8 +19,29 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         cardsArray = model.getCard()
+        
+        //Set the view controller as the dataSource & the delegete of the collection view
+        collectionView.dataSource = self
+        collectionView.delegate = self
     }
-
+//MARK: Collection View Delegete Methods
+    //the collection view is asking the view controller how many items i should display
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        //return number of cards
+        cardsArray.count
+    }
+    //the index path tells you exactly which cell in the collection view that we are talking about
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+       //get a cell
+        // try to look for a cell that it can reuse or creating a new one and return it
+       let cell =  collectionView.dequeueReusableCell(withReuseIdentifier: "cardCell", for: indexPath)
+        
+        // TODO: configre it
+        
+        
+        // return it
+        return cell
+    }
 
 }
 
